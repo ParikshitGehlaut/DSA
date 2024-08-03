770de8e420d6939453fc537ff0cb6a3c5da35ef4
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-08-03 14:35:41 +0530

    constructor in cpp

---
+++ C++/oops_6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+public:
+    Complex()
+    {
+        cout << "Hey! I am the default constructor of class Complex" << endl;
+        a = 10;
+        b = 0;
+    }
+
+    Complex(const Complex &other)
+    {
+        cout << "Hey! I am the copy constructor of class Complex" << endl;
+        a = other.a;
+        b = other.b;
+    }
+
+    void printNumber()
+    {
+        cout << "Your complex number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+int main()
+{
+    Complex c1;
+    c1.printNumber();
+
+    Complex c2 = c1; // This will invoke the copy constructor
+    c2.printNumber();
+
+    return 0;
+}
